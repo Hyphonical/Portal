545ee2630abf6bff540088f1ea52a21c797b0810
hyphonic <hyphonical@gmail.com>
2025-08-23 15:27:21 +0200

    style: Refactor code formatting for consistency and readability across multiple files

--- pages/join.vue
+++ pages/join.vue
@@ -1,6 +1,12 @@
 <template>
-  <section>
-    <h1 class="text-2xl font-semibold mb-4">Get Started</h1>
-    <p class="opacity-80">Welcome to Void Tales. This is a starter page.</p>
-  </section>
+	<section>
+		<h1 class="text-2xl font-semibold mb-4">Join Us</h1>
+		<p class="opacity-80">Welcome to Void Tales. This is a starter page.</p>
+	</section>
 </template>
+
+<script setup lang="ts">
+definePageMeta({
+	title: 'Join Us'
+})
+</script>
